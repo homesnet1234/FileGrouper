410b8ccc76253b217246878daf24cf02220c03a7
Magic <homesnet1234@hotmail.com>
2017-10-31 14:31:51 +0700

    Complete ver.beta

---
+++ FileGrouper/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FileGrouper
+{
+    partial class fileGrouper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.group = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // group
+            // 
+            this.group.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.group.Location = new System.Drawing.Point(17, 67);
+            this.group.Name = "group";
+            this.group.Size = new System.Drawing.Size(255, 40);
+            this.group.TabIndex = 0;
+            this.group.Text = "Group";
+            this.group.UseVisualStyleBackColor = true;
+            this.group.Click += new System.EventHandler(this.group_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click to Group Files";
+            // 
+            // fileGrouper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 122);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.group);
+            this.Name = "fileGrouper";
+            this.Text = "FileGrouper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button group;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileGrouper/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileGrouper
+{
+    public partial class fileGrouper : Form
+    {
+        public fileGrouper()
+        {
+            InitializeComponent();
+        }
+
+        private void group_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            string path = folderBrowserDialog.SelectedPath;
+
+            foreach (string f in Directory.GetFiles(path))
+            {
+                Console.Out.WriteLine(f);
+                string[] buffer = f.Split('.');
+                string fileName = f.Substring(f.LastIndexOf('\\'));
+
+                if (buffer.Length == 1)
+                {
+                    Directory.CreateDirectory(path + "\\Other");
+                    File.Move(f, @"" + path + "\\Other\\" + fileName);
+                }
+                else
+                {
+                    Directory.CreateDirectory(path + "\\" + buffer[buffer.Length - 1]);
+                    File.Move(f, @"" + path + "\\" + buffer[buffer.Length - 1] + "\\" + fileName);
+                }
+            }
+        }
+    }
+}
